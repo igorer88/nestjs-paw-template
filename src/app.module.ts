3919d1c4fa962c7d6988b49550fc295df6153a79
Igor Escalona <igorer88@gmail.com>
2025-01-16 01:17:38 -0400

    Add error handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module, Scope } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
-import { APP_INTERCEPTOR } from '@nestjs/core'
+import { APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core'
 
 import { apiConfig, dbPgConfig, getValidationSchema } from './config'
+import { ErrorService } from './config/errors/error.service'
+import { AllExceptionsFilter } from './config/errors/exception.filter'
 import { LoggingInterceptor } from './config/interceptors'
 import { DatabaseModule } from './database/database.module'
 
@@ -20,7 +22,12 @@ import { DatabaseModule } from './database/database.module'
       provide: APP_INTERCEPTOR,
       scope: Scope.REQUEST,
       useClass: LoggingInterceptor
-    }
+    },
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter
+    },
+    ErrorService
   ]
 })
 export class AppModule {
