dc9fa4809450a0fcd7c10860dba3a59d1edb9027
Igor Escalona <igorer88@gmail.com>
2025-01-16 00:56:52 -0400

    Add DB_DRIVER to validation schema

--- src/config/environments/validation.schema.ts
+++ src/config/environments/validation.schema.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi'
 
-import { Environment } from '../enum'
+import { Environment } from '../enums'
 
 export const getValidationSchema = (): Joi.ObjectSchema => {
   return Joi.object({
@@ -10,6 +10,7 @@ export const getValidationSchema = (): Joi.ObjectSchema => {
     API_PORT: Joi.number().default(3000),
     API_SECRET_KEY: Joi.string().required(),
     // DB credentials
+    DB_DRIVER: Joi.string().required(),
     DB_USER: Joi.string().required(),
     DB_PASSWORD: Joi.string().required(),
     DB_NAME: Joi.string().required(),
