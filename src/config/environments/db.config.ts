f5c02ef7f98d8864ed64ad93876e786bb5b2aaca
Igor Escalona <igorer88@users.noreply.github.com>
2025-06-05 21:18:55 -0400

    Rename db.pg.config to db.config

---
+++ src/config/environments/db.config.ts
@@ -0,0 +1,22 @@
+import { registerAs } from '@nestjs/config'
+
+/**
+ * Database configuration for the application.
+ * This configuration supports any database driver,
+ * add the specific configuration for each driver
+ * in the respective object.
+ *  e.g., mysql: { ... }
+ */
+export const dbConfig = registerAs('db', () => ({
+  driver: process.env.DB_DRIVER,
+  sqlite: {
+    database: process.env.DB_SQLITE_PATH
+  },
+  pg: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+  }
+}))
