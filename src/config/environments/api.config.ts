f8d96896a82bcddce389e3547a066278a841c55d
Igor Escalona <igorer88@users.noreply.github.com>
2025-06-06 00:48:14 -0400

    Set API_PORT undefined if not present

--- src/config/environments/api.config.ts
+++ src/config/environments/api.config.ts
@@ -2,6 +2,6 @@ import { registerAs } from '@nestjs/config'
 
 export const apiConfig = registerAs('api', () => ({
   environment: process.env.NODE_ENV,
-  port: parseInt(process.env.API_PORT),
+  port: process.env.API_PORT ? parseInt(process.env.API_PORT) : undefined,
   secretKey: process.env.API_SECRET_KEY
 }))
